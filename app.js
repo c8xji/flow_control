15cb292174f8a55ec4559e5d963f75cfdc1fa4cc
c8xji <rex14725@gmail.com>
2022-11-06 10:51:39 +0800

    finish q1 and q2

---
+++ app.js
@@ -0,0 +1,26 @@
+// question 1
+let num1 = 17;
+if (num1 % 2 === 0) {
+  console.log("This is an even number.");
+} else if (num1 % 2 === 1) {
+  console.log("This is an odd number.");
+} else {
+  console.log("error: num1 is not an integer.");
+}
+
+// question 2
+let numA = 600/3.14;
+let numB = 5*49/1.2;
+if (numA > numB) {
+  console.log("numA is the bigger one.");
+} else if (numA === numB) {
+  console.log("they have the same value.");
+} else {
+  console.log("numB is the bigger one");
+}
+
+// question 3
+
+// question 4
+// question 5
+// question 6
